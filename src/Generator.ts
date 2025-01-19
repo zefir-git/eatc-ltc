07d792ebad1d160c01929c3d02ce8172e674f61c
Zefir Kirilov <contact@zefir.pro>
2025-01-19 16:56:59 +0200

    generator class

---
+++ src/Generator.ts
@@ -0,0 +1,96 @@
+import Airspace from "./Airspace.js";
+import Airport from "./Airport.js";
+import Beacon from "./Beacon.js";
+import NamedFix from "./NamedFix.js";
+
+export default class Generator {
+	#airspace: Airspace | null = null;
+
+	/**
+	 * Get airspace.
+	 */
+	public airspace(): Airspace;
+
+	/**
+	 * Set airspace.
+	 */
+	public airspace(airspace: Airspace): typeof this;
+	public airspace(airspace?: Airspace): Airspace | typeof this {
+		if (airspace !== undefined) {
+			this.#airspace = airspace;
+			return this;
+		}
+		if (this.#airspace === null)
+			throw new Error("No airspace set");
+		return this.#airspace!;
+	}
+
+	/**
+	 * Get beacon by name.
+	 */
+	public beacon(name: string): Beacon {
+		const beacon = this.airspace().beacons.get(name);
+		if (beacon === undefined)
+			throw new Error(`Beacon ${name} not found`);
+		return beacon;
+	}
+
+	/**
+	 * Get beacons by name. Non-existent beacons are ignored.
+	 */
+	public beacons(...names: string[]): Beacon[] {
+		return names.map(this.airspace().beacons.get)
+					.filter(n => n !== undefined) as Beacon[];
+	}
+
+	#airports: Map<string, Airport> = new Map();
+
+	/**
+	 * Get airport.
+	 * @param code ICAO code.
+	 */
+	public airport(code: string): Airport;
+	/**
+	 * Add airport.
+	 */
+	public airport(airport: Airport): typeof this;
+	public airport(a: Airport | string): Airport | typeof this {
+		if (typeof a === "string") {
+			const airport = this.#airports.get(a);
+			if (airport === undefined)
+				throw new Error(`Airport ${a} not found`);
+			return airport;
+		}
+		this.#airports.set(a.code, a);
+		return this;
+	}
+
+	/**
+	 * Get a fix by name/identifier.
+	 */
+	public fix(name: string): NamedFix {
+		const beacon = this.airspace().beacons.get(name);
+		if (beacon !== undefined)
+			return beacon;
+		for (const airport of this.#airports.values()) {
+			const sid = airport.sidMarkers.get(name);
+			if (sid !== undefined)
+				return sid;
+		}
+		throw new Error(`Fix ${name} not found`);
+	}
+
+	/**
+	 * Generate Endless ATC configuration
+	 * @param [header] Data to prepend. Prefix lines with # for comments.
+	 */
+	public generate(header?: string): string {
+		return [
+			header?.trim() ?? null,
+			this.airspace().toString(),
+			Array.from(this.#airports.values())
+				 .map((airport, i) => `[airport${i + 1}]\n${airport.toString()}`)
+				 .join("\n")
+		].filter(l => l !== null).join("\n\n");
+	}
+}
