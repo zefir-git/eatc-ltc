4894bbe5236abc923d7e945b7ae98e41fb838309
Zefir Kirilov <contact@zefir.pro>
2025-01-20 13:06:23 +0200

    London Heathrow airport (with STARs)

---
+++ airspace/EGLL.ts
@@ -0,0 +1,237 @@
+import Generator from "../src/Generator.js";
+import STAR from "../src/STAR.js";
+import StarFix from "../src/StarFix.js";
+import Airport from "../src/Airport.js";
+import Runway from "../src/Runway.js";
+import Fix from "../src/Fix.js";
+
+export default class EGLL {
+	public constructor(private readonly atc: Generator) {
+		this.airport();
+		this.star();
+	}
+
+	private airport() {
+		this.atc.airport(
+			new Airport(
+				"London Heathrow Airport",
+				"Heathrow",
+				"TEST",
+				80,
+				6000,
+				[
+					new Runway("lln", "27R",
+						Fix.fromDMS("512839.63N", "0002559.82W"),
+						269.71, 77,
+						3901 / Fix.FT, 0, 309 / Fix.FT,
+						{name: "IVLAR", distance: 10},
+						{name: "ABAVI", distance: 10},
+						118.7,
+						"Tower",
+						89.67
+					),
+					new Runway("lls", "27L",
+						Fix.fromDMS("512753.82N", "0002602.76W"),
+						269.72, 75,
+						3658 / Fix.FT, 0, 308 / Fix.FT,
+						{name: "NEKSA", distance: 10},
+						{name: "BENPA", distance: 10},
+						118.5,
+						"Tower",
+						89.68
+					),
+				],
+				[],
+				[
+					new Airport.Airline("BAW", 10, ["a320"], ["e"], "Speedbird"),
+				],
+				[],
+				this.atc.beacon("LON")
+			)
+		);
+	}
+
+	private star() {
+		this.atc.arrival(new STAR(
+			...STAR.named("OTMET1H"),
+			[this.atc.runway("lln"), this.atc.runway("lls")],
+			this.atc.beacon("BEGTO"),
+			63,
+			[
+				this.atc.beacon("BEGTO"),
+				this.atc.beacon("HAZEL", 13000),
+				this.atc.fix("LLS01", "511021.99N", "0004108.43W", void 0, 250),
+				this.atc.beacon("OCK", 7000, 220)
+			],
+			{end: "hold"}
+		));
+
+		this.atc.arrival(new STAR(
+			...STAR.named("ROXOG1H"),
+			[this.atc.runway("lln"), this.atc.runway("lls")],
+			this.atc.beacon("BEGTO"),
+			17,
+			[
+				this.atc.beacon("BEGTO"),
+				this.atc.beacon("HAZEL", 13000),
+				this.atc.fix("LLS01", "511021.99N", "0004108.43W", void 0, 250),
+				this.atc.beacon("OCK", 7000, 220)
+			],
+			{end: "hold"}
+		));
+
+		this.atc.arrival(new STAR(
+			...STAR.named("ALESO1H"),
+			[this.atc.runway("lln"), this.atc.runway("lls")],
+			this.atc.beacon("ROTNO"),
+			314,
+			[
+				this.atc.beacon("ROTNO"),
+				this.atc.fix("ETVAX", "505806.99N", "0003556.27E", 18000),
+				this.atc.beacon("TIGER"),
+				this.atc.fix("LLE01", "511113.96N", "0001521.68E", void 0, 250),
+				this.atc.beacon("BIG", 7000, 220)
+			],
+			{end: "hold"}
+		));
+
+		this.atc.arrival(new STAR(
+			...STAR.named("ALESO1H"),
+			[this.atc.runway("lln"), this.atc.runway("lls")],
+			this.atc.beacon("TIGER"),
+			314,
+			[
+				this.atc.beacon("TIGER"),
+				this.atc.fix("LLE01", "511113.96N", "0001521.68E", void 0, 250),
+				StarFix.from(this.atc.beacon("BIG"), 7000, 220)
+			],
+			{end: "hold"}
+		));
+
+		// LAM 1X omitted (LAM→BIG)
+
+		this.atc.arrival(new STAR(
+			...STAR.named("TANET1Z"),
+			[this.atc.runway("lln"), this.atc.runway("lls")],
+			this.atc.beacon("TANET"),
+			void 0,
+			[
+				this.atc.beacon("TANET"),
+				this.atc.fix("DET", "511814.41N", "0003550.19E"),
+				this.atc.fix("LLE02", "511857.68N", "0002109.87E", void 0, 250),
+				this.atc.beacon("BIG", 7000, 220)
+			],
+			{end: "hold"}
+		));
+
+		// OCK 1Z omitted (OCK→BIG)
+
+		this.atc.arrival(new STAR(
+			...STAR.named("NUGRA1H"),
+			[this.atc.runway("lln"), this.atc.runway("lls")],
+			this.atc.beacon("TOBID"),
+			147,
+			[
+				this.atc.beacon("TOBID"),
+				this.atc.fix("SOPIT", 15000),
+				this.atc.beacon("WCO", void 0, 220),
+				this.atc.beacon("BNN", 7000, 220)
+			],
+			{end: "hold"}
+		));
+
+		// LAM 1Z omitted (LAM→BNN)
+
+		this.atc.arrival(new STAR(
+			"HON1H",
+			"Honiley one hotel",
+			[this.atc.runway("lln"), this.atc.runway("lls")],
+			this.atc.beacon("TOBID"),
+			139,
+			[
+				this.atc.beacon("TOBID"),
+				this.atc.fix("SOPIT", 15000),
+				this.atc.beacon("WCO", void 0, 220),
+				this.atc.beacon("BNN", 7000, 220)
+			],
+			{end: "hold"}
+		));
+
+		this.atc.arrival(new STAR(
+			...STAR.named("BARMI1H"),
+			[this.atc.runway("lln"), this.atc.runway("lls")],
+			this.atc.beacon("LOGAN"),
+			228,
+			[
+				this.atc.beacon("LOGAN", 25000),
+				this.atc.fix("SABER", "514213.76N", "0005658.19E", 16000),
+				this.atc.fix("BRASO", "514106.57N", "0004100.03E"),
+				this.atc.fix("WESUL", "514015.29N", "0002909.27E", void 0, 250),
+				this.atc.beacon("LAM", 7000, 250)
+			],
+			{end: "hold"}
+		));
+
+		this.atc.arrival(new STAR(
+			...STAR.named("LOGAN2H"),
+			[this.atc.runway("lln"), this.atc.runway("lls")],
+			this.atc.beacon("LOGAN"),
+			void 0,
+			[
+				this.atc.beacon("LOGAN", 25000),
+				this.atc.fix("SABER", "514213.76N", "0005658.19E", 16000),
+				this.atc.fix("BRASO", "514106.57N", "0004100.03E"),
+				this.atc.fix("WESUL", "514015.29N", "0002909.27E", void 0, 250),
+				this.atc.beacon("LAM", 7000, 250)
+			],
+			{end: "hold"}
+		));
+
+		// TOBID 1X omitted (TOBID→OCK)
+
+		this.atc.arrival(new STAR(
+			...STAR.named("HAZEL1H"),
+			[this.atc.runway("lln"), this.atc.runway("lls")],
+			this.atc.beacon("HAZEL"),
+			void 0,
+			[
+				this.atc.beacon("HAZEL", 13000),
+				this.atc.fix("LLS01", "511021.99N", "0004108.43W", void 0, 250),
+				this.atc.beacon("OCK", 7000, 220)
+			],
+			{end: "hold"}
+		));
+
+		// BIG 1Z omitted (BIG→OCK)
+		// LAM 1Y omitted (LAM→OCK)
+
+		this.atc.arrival(new STAR(
+			...STAR.named("FITBO1H"),
+			[this.atc.runway("lln"), this.atc.runway("lls")],
+			this.atc.beacon("SOPIT"),
+			107,
+			[
+				this.atc.fix("SOPIT", 15000),
+				this.atc.beacon("WCO", void 0, 220),
+				this.atc.beacon("BNN", 7000, 220)
+			],
+			{end: "hold"}
+		));
+
+		this.atc.arrival(new STAR(
+			...STAR.named("SIRIC1H"),
+			[this.atc.runway("lln"), this.atc.runway("lls")],
+			this.atc.beacon("SIRIC"),
+			void 0,
+			[
+				this.atc.beacon("SIRIC", 14000),
+				this.atc.fix("NIGIT", "511846.96N", "0011014.71W"),
+				this.atc.fix("LLW03", "511832.83N", "0004556.94W", void 0, 250),
+				this.atc.beacon("OCK", 7000, 220)
+			],
+			{end: "hold"}
+		));
+
+		// SIRIC 1Z omitted (SIRIC→BNN)
+	}
+}
---
+++ airspace/index.ts
@@ -0,0 +1,150 @@
+import fs from "node:fs/promises";
+import Generator from "../src/Generator.js";
+import Airspace from "../src/Airspace.js";
+import Fix from "../src/Fix.js";
+import Beacon from "../src/Beacon.js";
+import Polygon from "../src/Polygon.js";
+import RunwayConfiguration from "../src/RunwayConfiguration.js";
+import EGLL from "./EGLL.js";
+
+const atc = new Generator();
+
+atc.airspace(
+	new Airspace(
+		50,
+		5,
+		36,
+		1300,
+		{altitude: {ft: 10000, kts: 250}},
+		{nm: 4, kts: 160},
+		19000,
+		20000,
+		22000,
+		3000,
+		6000,
+		false,
+		false,
+		2,
+		4,
+		true,
+		true,
+		{
+			approach: "London Control",
+			departure: "London Control",
+		},
+		atc.fix("LON", "512914N", "0002800W"),
+		0,
+		[
+			Beacon.from("LON", "London", atc.fix("LON")),
+			Beacon.fromDMS("513845.69N", "0000906.13E", "LAM", "Lambourne", {left: 263}),
+			Beacon.fromDMS("511951.15N", "0000205.32E", "BIG", "Biggin", 302),
+			Beacon.fromDMS("511818.17N", "0002649.86W", "OCK", "Ockham", 328),
+			Beacon.fromDMS("514334.19N", "0003259.10W", "BNN", "Bovingdon", 116),
+			//new Beacon("BKY", "Barkway", 51.989722, 0.061944),
+
+			Beacon.fromDMS("510401.82N", "0002621.54E", "TIGER", "Tiger", 315),
+			Beacon.fromDMS("515110.51N", "0005744.67W", "WCO", "Westcott", 188),
+
+			// STAR entries
+			Beacon.fromDMS("504545.12N", "0011407.99W", "BEGTO", "Begto"),
+			Beacon.fromDMS("505126.07N", "0004641.20E", "ROTNO", "Rotno"),
+			Beacon.fromDMS("512658.00N", "0005531.99E", "TANET", "Tanet"),
+			Beacon.fromDMS("515729.84N", "0010626.35W", "SOPIT", "Sopit"),
+			/**
+			 * TOBID: moved closer to sopit to fit within airspace.
+			 * Use this beacon only for NUGRA 1H HON 1H
+			 */
+			Beacon.from("TOBID", "Tobid",
+				atc.fix("SOPIT", "515729.84N", "0010626.35W")
+				   .destination(319, 10)
+			),
+			Beacon.fromDMS("514451.32N", "0013642.58E", "LOGAN", "Logan", {left: 289}),
+			Beacon.fromDMS("510019.03N", "0005904.06W", "HAZEL", "Hazel"),
+			Beacon.fromDMS("512036.17N", "0013358.89W", "SIRIC", "Siric"),
+		],
+		new Map([
+			[180, {callsign: "London Control", pronunciation: "London Control", frequency: 129.425}],
+			[270, {callsign: "London Control", pronunciation: "London Control", frequency: 126.075}],
+			[360, {callsign: "London Control", pronunciation: "London Control", frequency: 127.1}],
+			[90, {callsign: "Amsterdam Radar", pronunciation: "Amsterdam Radar", frequency: 134.375}],
+		]),
+		new Polygon([
+				new Fix(50.672965, -0.369415),
+				new Fix(50.672965, 0.502625),
+				new Fix(50.754704, 0.748444),
+				new Fix(50.979182, 1.040955),
+				new Fix(51.647850, 1.416550),
+				new Fix(51.871828, 1.443329),
+				new Fix(51.985726, 1.383591),
+				new Fix(51.931988, 1.118546),
+				new Fix(51.973038, 0.965424),
+				new Fix(52.212656, 0.806808),
+				new Fix(52.191193, 0.547943),
+				new Fix(52.186142, -0.366154),
+				new Fix(52.052174, -0.581074),
+				new Fix(52.068533, -0.622101),
+				new Fix(51.951671, -1.196823),
+				new Fix(51.731387, -1.018295),
+				new Fix(51.562025, -1.200256),
+				new Fix(51.587310, -1.438179),
+				new Fix(51.424260, -1.489677),
+				new Fix(51.385924, -1.146698),
+				new Fix(51.051539, -1.224461),
+				new Fix(51.066320, -1.121120),
+				new Fix(50.907688, -1.062927),
+				new Fix(50.821879, -1.284542),
+				new Fix(50.814504, -0.873585),
+				new Fix(50.775767, -0.829811),
+				new Fix(50.777938, -0.486145),
+			]
+		),
+	)
+);
+
+new EGLL(atc);
+
+atc.runway([
+	new RunwayConfiguration(0, atc.runway("lls"), {arrivals: true}),
+	new RunwayConfiguration(0, atc.runway("lln"), {departures: true}),
+]);
+atc.runway([
+	new RunwayConfiguration(0, atc.runway("lls"), {departures: true}),
+	new RunwayConfiguration(0, atc.runway("lln"), {arrivals: true}),
+]);
+atc.runway([
+	new RunwayConfiguration(0, atc.runway("lls"), {arrivals: true}),
+	new RunwayConfiguration(0, atc.runway("lln"), {departures: true}),
+	new RunwayConfiguration(23, atc.runway("lln"), {arrivals: true, departures: true}),
+]);
+atc.runway([
+	new RunwayConfiguration(0, atc.runway("lls"), {departures: true}),
+	new RunwayConfiguration(0, atc.runway("lln"), {arrivals: true}),
+	new RunwayConfiguration(23, atc.runway("lls"), {arrivals: true, departures: true}),
+]);
+
+const project = JSON.parse(await fs.readFile("./package.json", "utf8"));
+
+console.log(atc.generate(`
+#
+# London Terminal Control for EndlessATC
+# Version: ${project.version}
+# ${project.repository.url}
+# eatc+ltc@zefir.pro
+#
+# !! THIS FILE IS GENERATED !! Please do not make modifications here.
+#
+# Copyright © ${new Date().getFullYear()} ${project.author}.
+# This file is part of ${project.name}.
+# This custom airspace is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# This file is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this file. If not, see <https://www.gnu.org/licenses/>.
+#`));
