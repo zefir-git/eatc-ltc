12f8afc874033de991f6273cb8ff3dd298c9d3fc
Zefir Kirilov <contact@zefir.pro>
2025-01-27 11:25:23 +0200

    make polygon vertices modifiable in protected context

--- src/Polygon.ts
+++ src/Polygon.ts
@@ -4,13 +4,20 @@ import Fix from "./Fix.js";
  * A basic polygon made from straight lines connecting fixes.
  */
 export default class Polygon {
+	protected _vertices: Fix[];
 	/**
 	 * Create new polygon. Last vertex is automatically connected to first vertex.
 	 * @param vertices The vertices of the polygon.
 	 */
 	public constructor(
-		public readonly vertices: ReadonlyArray<Fix>
-	) {}
+		vertices: Iterable<Fix>
+	) {
+		this._vertices = [...vertices];
+	}
+
+	public get vertices(): ReadonlyArray<Fix> {
+		return this._vertices;
+	}
 
 	public toString(): string {
 		return this.vertices.map(vertex => "\t" + vertex.toString()).join("\n");
