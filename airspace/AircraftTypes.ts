37fa757811ee18e12bf55eca8e8e8b82336bcb42
Zefir Kirilov <contact@zefir.pro>
2025-07-24 10:53:34 +0300

    added 12 aircraft types

--- airspace/AircraftTypes.ts
+++ airspace/AircraftTypes.ts
@@ -60,5 +60,173 @@ export default class AircraftTypes {
             [3, 5],
             [2.9, 3.1],
         ));
+
+        gen.aircraft(new Aircraft(
+            "RF6",
+            "Slingsby",
+            Aircraft.WTC.LIGHT,
+            [80, 150],
+            [60, 65],
+            [1.1, 1.2],
+            [1400, 1600],
+            [1900, 2100],
+            [25, 30],
+            [5, 7],
+            [2.9, 3.1]
+        ));
+
+        gen.aircraft(new Aircraft(
+            "PUP",
+            "Beagle",
+            Aircraft.WTC.LIGHT,
+            [80, 120],
+            [65, 70],
+            [1.1, 1.2],
+            [1400, 1600],
+            [1900, 2100],
+            [25, 30],
+            [5, 7],
+            [2.9, 3.1]
+        ));
+
+        gen.aircraft(new Aircraft(
+            "EV97",
+            "Evektor",
+            Aircraft.WTC.LIGHT,
+            [80, 100],
+            [60, 65],
+            [1.1, 1.2],
+            [1400, 1600],
+            [1900, 2100],
+            [25, 30],
+            [5, 7],
+            [2.9, 3.1]
+        ));
+
+        gen.aircraft(new Aircraft(
+            "D140",
+            "Jodel",
+            Aircraft.WTC.LIGHT,
+            [80, 130],
+            [60, 65],
+            [1.1, 1.2],
+            [1400, 1600],
+            [1900, 2100],
+            [25, 30],
+            [5, 7],
+            [2.9, 3.1]
+        ));
+
+        gen.aircraft(new Aircraft(
+            "C42",
+            "Ikarus",
+            Aircraft.WTC.LIGHT,
+            [80, 105],
+            [55, 60],
+            [1.1, 1.2],
+            [1400, 1600],
+            [1900, 2100],
+            [25, 30],
+            [5, 7],
+            [2.9, 3.1]
+        ));
+
+        gen.aircraft(new Aircraft(
+            "DV20",
+            "Diamond",
+            Aircraft.WTC.LIGHT,
+            [85, 160],
+            [65, 70],
+            [1.1, 1.2],
+            [1400, 1600],
+            [2000, 2200],
+            [25, 30],
+            [5, 7],
+            [2.9, 3.1]
+        ));
+
+        gen.aircraft(new Aircraft(
+            "EA50",
+            "Eclipse",
+            Aircraft.WTC.LIGHT,
+            [160, 300],
+            110,
+            [1.2, 1.4],
+            1400,
+            3620,
+            [25, 30],
+            [3, 5],
+            [2.9, 3.1],
+        ));
+
+        gen.aircraft(new Aircraft(
+            "M600",
+            "Piper",
+            Aircraft.WTC.LIGHT,
+            [130, 270],
+            [110, 120],
+            [1.1, 1.2],
+            [1400, 1600],
+            [2000, 2200],
+            [25, 30],
+            [5, 7],
+            [2.9, 3.1]
+        ));
+
+        gen.aircraft(new Aircraft(
+            "DA62",
+            "Diamond",
+            Aircraft.WTC.LIGHT,
+            [110, 190],
+            [90, 95],
+            [1.1, 1.2],
+            [1400, 1600],
+            [2000, 2200],
+            [25, 30],
+            [5, 7],
+            [2.9, 3.1]
+        ));
+
+        gen.aircraft(new Aircraft(
+            "P180",
+            "Piaggio",
+            Aircraft.WTC.LIGHT,
+            [160, 300],
+            [130, 145],
+            [1.1, 1.2],
+            [1400, 1600],
+            [2000, 3000],
+            [25, 30],
+            [5, 7],
+            [2.9, 3.1]
+        ));
+
+        gen.aircraft(new Aircraft(
+            "PA34",
+            "Piper",
+            Aircraft.WTC.LIGHT,
+            [140, 160],
+            [100, 110],
+            [1.1, 1.2],
+            [1400, 1600],
+            [2000, 2200],
+            [25, 30],
+            [5, 7],
+            [2.9, 3.1]
+        ));
+
+        gen.aircraft(new Aircraft(
+            "DA42",
+            "Diamond",
+            Aircraft.WTC.LIGHT,
+            [110, 200],
+            [85, 90],
+            [1.1, 1.2],
+            [1400, 1600],
+            [2000, 2200],
+            [25, 30],
+            [5, 7],
+            [2.9, 3.1]
+        ));
     }
 }
